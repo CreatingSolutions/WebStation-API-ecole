7f2e40106a3500f4233211f8135b38d7dbab5ca4
place <place@b13p22.fil.univ-lille1.fr>
2018-10-03 10:29:37 +0200

    feat/base : added annotations and updated pom.xml to use Lombok for getters/setters

--- src/main/java/webstationapi/Entity/Booking.java
+++ src/main/java/webstationapi/Entity/Booking.java
@@ -2,32 +2,48 @@ package webstationapi.Entity;
 
 import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Booking {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "BOOKING_ID")
 	private int bookingId;
+	
+	@Column(name = "USER_ID")
 	private int userId;
+	
+	@OneToOne
+	@JoinColumn(name = "TEACHER_ID")
 	private Teacher teacher;
+	
+	@OneToOne
+	@JoinColumn(name = "COURSE_ID")
 	private Course course;
+	
+	@Column(name = "PRICE")
 	private int price;
+	
+	@Column(name = "DATE")
 	private Date date;
+	
+	@Column(name = "STARTING_HOUR")
 	private int startingHour;
+	
+	@Column(name = "LEVEL")
 	private int level;
-	
-	public int getBookingId() { return bookingId; }
-	public void setBookingId(int bookingId) { this.bookingId = bookingId; }
-	public int getUserId() { return userId; }
-	public void setUserId(int userId) { this.userId = userId; }
-	public Teacher getTeacher() { return teacher; }
-	public void setTeacher(Teacher teacher) { this.teacher = teacher; }
-	public Course getCourse() { return course; }
-	public void setCourse(Course course) { this.course = course; }
-	public int getPrice() { return price; }
-	public void setPrice(int price) { this.price = price; }
-	public Date getDate() { return date; }
-	public void setDate(Date date) { this.date = date; }
-	public int getStartingHour() { return startingHour; }
-	public void setStartingHour(int startingHour) { this.startingHour = startingHour; }
-	public int getLevel() { return level; }
-	public void setLevel(int level) { this.level = level; }
-	
+
 }
