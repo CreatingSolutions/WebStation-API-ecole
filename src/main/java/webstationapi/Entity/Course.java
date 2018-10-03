ef9718f373aad84c5d5d8b45e5fcda9b7319c07d
place <place@b13p22.fil.univ-lille1.fr>
2018-10-03 10:29:37 +0200

    feat/base : added annotations and updated pom.xml to use Lombok for getters/setters

--- src/main/java/webstationapi/Entity/Course.java
+++ src/main/java/webstationapi/Entity/Course.java
@@ -1,16 +1,28 @@
 package webstationapi.Entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Course {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "COURSE_ID")
 	private int courseId;
-	private String description;
-	private String type;
 	
-	public int getCourseId() { return courseId; }
-	public void setCourseId(int courseId) { this.courseId = courseId; }
-	public String getDescription() { return description; }
-	public void setDescription(String description) { this.description = description; }
-	public String getType() { return type; }
-	public void setType(String type) { this.type = type; }
+	@Column(name = "DESCRIPTION")
+	private String description;
+	
+	@Column(name = "TYPE")
+	private String type;
 
 }
