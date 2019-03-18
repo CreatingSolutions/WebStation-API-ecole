695a4f859b1e0f4d8856b59666d59fbf67aba189
place <place@b13p22.fil.univ-lille1.fr>
2019-03-18 18:29:42 +0100

    ecole advanced

--- src/main/java/webstationapi/Repository/TeacherRepository.java
+++ src/main/java/webstationapi/Repository/TeacherRepository.java
@@ -1,11 +1,13 @@
 package webstationapi.Repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import webstationapi.Entity.Teacher;
 
 public interface TeacherRepository extends CrudRepository<Teacher, Integer> {
 
-	// public List<Teacher> getTeacherByName(String name);
+	public List<Teacher> findByLastNameIsLike(String lastName);
 	
 }
